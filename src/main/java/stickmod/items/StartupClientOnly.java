0e9854cacfe81ddf7423a44a2a9dc2e79429a53d
TheGreyGhost <spamblackhole88@yahoo.com.au>
2015-05-29 20:49:42 +0930

    Basics in place

--- src/main/java/stickmod/items/StartupClientOnly.java
+++ src/main/java/stickmod/items/StartupClientOnly.java
@@ -1,20 +1,14 @@
 package stickmod.items;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * User: The Grey Ghost
  * Date: 24/12/2014
  *
- * The Startup classes for this example are called during startup, in the following order:
- *  preInitCommon
- *  preInitClientOnly
- *  initCommon
- *  initClientOnly
- *  postInitCommon
- *  postInitClientOnly
- *  See MinecraftByExample class for more information
  */
 public class StartupClientOnly
 {
@@ -24,13 +18,38 @@ public class StartupClientOnly
 
   public static void initClientOnly()
   {
-    // required in order for the renderer to know how to render your item.  Likely to change in the near future.
-    ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation("stickmod:items", "inventory");
     final int DEFAULT_ITEM_SUBTYPE = 0;
-    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(StartupCommon.itemLongStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+    ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+    ModelResourceLocation itemModelResourceLocation;
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_long_stick", "inventory");
+    mesher.register(StartupCommon.itemLongStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_extra_long_stick", "inventory");
+    mesher.register(StartupCommon.itemExtraLongStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_fat_stick", "inventory");
+    mesher.register(StartupCommon.itemFatStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_coal_stick", "inventory");
+    mesher.register(StartupCommon.itemCoalStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_stone_stick", "inventory");
+    mesher.register(StartupCommon.itemStoneStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_iron_stick", "inventory");
+    mesher.register(StartupCommon.itemIronStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_gold_stick", "inventory");
+    mesher.register(StartupCommon.itemGoldStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_diamond_stick", "inventory");
+    mesher.register(StartupCommon.itemDiamondStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+
+    itemModelResourceLocation = new ModelResourceLocation("stickmod:item_emerald_stick", "inventory");
+    mesher.register(StartupCommon.itemEmeraldStick, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
   }
 
-public static void postInitClientOnly()
+  public static void postInitClientOnly()
   {
   }
 }
